b78b7a403652a7872d1cff3cfce1b6d272bcc0d9
d5aint <mike@dsaint.net>
2024-08-25 02:34:33 +0000

    Backup triggered on 2024-08-25 02:34:33

--- stage3/01-install-packages/01-run.sh
+++ stage3/01-install-packages/01-run.sh
@@ -2,24 +2,7 @@
 
 #install -m 644 files/tft35a.dtbo "${ROOTFS_DIR}/boot/firmware/overlays/"
 
-#install -m 644 files/interfaces-local "${ROOTFS_DIR}/etc/network/interfaces.d/"
-
-install -v -m 0755 -o 1000 -g 1000 -d "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}"/.config/openbox
-install -m 755 -o 1000 -g 1000 files/autostart "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}/.config/openbox/"
- 
-#HOME="${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
-#mkdir -p "${HOME}/.config/openbox/"
-#install -m 755 -o 1000 -g 1000 files/.xinitrc "${HOME}/"
-#install -m 755 -o 1000 -g 1000 files/.xsession "${HOME}/"
-
-install -m 644 -o 1000 -g 1000 files/rc.xml "${HOME}/.config/openbox/"
-
-#on_chroot << EOF
-#SUDO_USER="${FIRST_USER_NAME}" python -m venv --system-site-packages .env
-#EOF
-
 on_chroot << EOF
-#systemctl enable ifplugd
 usermod -a -G tcpdump pi
 EOF
 
@@ -29,17 +12,6 @@ EOF
 #chown -R tftp:tftp "/srv/tftp"
 #chmod 774 "/srv/tftp"
 #EOF
-
-#install -m 644 files/custom.toml "${ROOTFS_DIR}/boot/firmware/"
-#FIRSTUSERPASS=`echo "$FIRST_USER_PASS" | openssl passwd -5 -stdin`
-
-#sed -i -e "s|TARGET_HOSTNAME|${TARGET_HOSTNAME}|g" \
-#  -e "s|FIRST_USER_NAME|${FIRST_USER_NAME}|" \
-#  -e "s|FIRST_USER_PASS|${FIRSTUSERPASS}|" \
-#  -e "s|PUBKEY_SSH_FIRST_USER|${PUBKEY_SSH_FIRST_USER}|" \
-#  -e "s|WPA_COUNTRY|${WPA_COUNTRY}|" \
-#  -e "s|KEYBOARD_KEYMAP|${KEYBOARD_KEYMAP}|" \
-#  -e "s|TIMEZONE_DEFAULT|${TIMEZONE_DEFAULT}|" "${ROOTFS_DIR}/boot/firmware/custom.toml"
 
 #on_chroot << EOF
 #loginctl enable-linger
--- stage4/00-install-packages/01-run.sh
+++ stage4/00-install-packages/01-run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
+install -v -m 644 files/40-libinput.conf "${ROOTFS_DIR}/etc/X11/xorg.conf.d/"
+
+install -v -m 0755 -o 1000 -g 1000 -d "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}"/.config/openbox
+install -v -m 755  -o 1000 -g 1000 files/autostart "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}/.config/openbox/"
+install -v -m 644  -o 1000 -g 1000 files/rc.xml "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}/.config/openbox/"
+#install -v -m 755 -o 1000 -g 1000 files/.xinitrc "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}/"
+#install -v -m 755 -o 1000 -g 1000 files/.xsession "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}/"
+
 on_chroot << EOF
-  apt-mark auto python3-pyqt5 python3-opengl
+SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_wayland W1
+EOF
+
+on_chroot << EOF
+SUDO_USER="${FIRST_USER_NAME}" python -m venv --system-site-packages "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}/.env"
 EOF
